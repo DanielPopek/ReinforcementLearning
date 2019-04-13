a77357a3ba56547efc25c38c375cf9de193149c8
daniel <daniel.popek13@gmail.com>
2019-04-13 12:03:35 +0200

    Players history tracing

--- game.py
+++ game.py
@@ -2,12 +2,16 @@ from board import Board
 from random_player import RandomPlayer
 class Game(object):
 
+    #Historia ma postać
+    # history=[(stateHash,action),(stateHash,action)..] np. [(233dsw2,2),(232345,1),...]
     def __init__(self,board,player1,player2):
         self.board=board
         self.player1=player1
         self.player2=player2
         self.playerActive=player1
         self.playerInactive=player2
+        self.player1History=[]
+        self.player2History=[]
 
     def switchPlayers(self):
         if self.playerActive==self.player1:
@@ -17,12 +21,24 @@ class Game(object):
             self.playerActive=self.player1
             self.playerInactive=self.player2
 
+    def putHistoryTuple(self,player,board,action):
+        sign=player.sign
+        history=self.player1History if player==player1 else self.player2History
+        hash=None
+        if sign==1:
+            hash=board.getHash()
+        else:
+            hash=board.getRevertHash()
+        history.append((hash,action))
+
+
     def playGame(self):
         move=0
         winner=-1
         while winner==-1 and not self.board.isFull():
             print(f'TURN: {move}')
             action=self.playerActive.nextMove()
+            self.putHistoryTuple(self.playerActive,self.board,action)
             self.board.board[action]=self.playerActive.sign
             self.switchPlayers()
             winner=board.getWinningSign()
@@ -31,15 +47,17 @@ class Game(object):
         self.board.printBoard()
         if(board.getWinningSign()!=-1):
             sign=self.board.getWinningSign()
-            print(f' THE WINNER IS: {self.getWinner(sign)} WITH SIGN {sign}')
+            print(f' THE WINNER IS: {self.getWinner(sign).name} WITH SIGN {sign}')
         else:
             print('DRAW')
+        print(self.player1History)
+        print(self.player2History)
 
     def getWinner(self,sign):
         if player1.sign==sign:
-            return player1.name
+            return player1
         else:
-            return player2.name
+            return player2
 
     def prepareSampleGame(self):
         board= Board()
--- q_learnig.py
+++ q_learnig.py
@@ -2,8 +2,8 @@
 class QLearning(object):
     #assumes that the state-action matrix is a dictionary <stateHash:string, actionsQ:real>
     #actions are in order row by row : [top-lef,top-middle,top-right,middle-left....,down-right]
-    #states are perceived from the perspective of CROSS
-    #in order to update state's Qs for Os, the board hash has to be inverted
+    #states are perceived from the perspective of CROSSes (sign 1)
+    #in order to update state's Qs for Os, the board hash has to be previously inverted --> in Game class
 
     ALPHA=0.9
     GAMMA=0.95
